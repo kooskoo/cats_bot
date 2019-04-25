6ec3208a42f1ebe63d6a065243cb0d9c83366c64
kooskoo <kos.kochetkov@gmail.com>
2019-04-25 12:46:54 +0300

    v0.2

--- bot.py
+++ bot.py
@@ -6,15 +6,7 @@ from telegram.ext import Updater, CommandHandler
 from telegram import ChatAction
 import logging
 
-import catapi
-
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-updater = Updater(os.environ['TG_TOKEN'], use_context=True)
-
-dispatcher = updater.dispatcher
+import cat_api
 
 
 def send_action(action):
@@ -41,17 +33,30 @@ def help_callback(update, context):
                                   'To get a specific breed - type /cat <breed>. '
                                   '<breed> is four-character ID. '
                                   'Here is the mapping of breed and their IDs:\n\n'
-                                  + str(catapi.get_breeds_list()))
+                                  + str(cat_api.get_breeds_list()))
 
 
 @send_action(ChatAction.UPLOAD_PHOTO)
 def cat_callback(update, context):
-    context.bot.send_photo(chat_id=update.message.chat_id, photo=catapi.cat_by_breed(context.args))
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=cat_api.cat_by_breed(context.args))
 
 
-dispatcher.add_handler(CommandHandler('start', start_callback))
-dispatcher.add_handler(CommandHandler('help', help_callback))
-dispatcher.add_handler(CommandHandler('cat', cat_callback))
+if __name__ == '__main__':
 
-updater.start_polling()
-updater.idle()
+    TOKEN = os.environ['TG_TOKEN']
+    NAME = 'cats-bot'
+    PORT = ''
+
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    updater = Updater(TOKEN, use_context=True)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start_callback))
+    dispatcher.add_handler(CommandHandler('help', help_callback))
+    dispatcher.add_handler(CommandHandler('cat', cat_callback))
+
+    updater.start_polling()
+    updater.idle()
